c5e97ef2962adcc480f7b8774df11a9ae809cf52
Enrico Höschler <enrico.hoeschler@gmail.com>
2018-03-12 12:42:24 +0100

    Add linting tools

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    //'eslint:recommended',
+    'plugin:vue/recommended'
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  },
+  env: {
+    amd: true
+  }
+}
