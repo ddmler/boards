c478e10498d0c218facdaca0acc77a69fe67d04c
Enrico Höschler <enrico.hoeschler@gmail.com>
2018-03-17 19:03:43 +0100

    Route all web routes to the spa

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::any('{all}', function () {
+    return view('app');
+})->where(['all' => '.*']);
