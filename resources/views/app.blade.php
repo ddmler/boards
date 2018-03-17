4f1c4810f641b96256dcc3517be838705e1a3c40
Enrico Höschler <enrico.hoeschler@gmail.com>
2018-03-17 19:26:59 +0100

    Create app view, run mix

---
+++ resources/views/app.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name') }}</title>
+
+        <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+        <script>
+            window.Laravel = {!! json_encode([
+                'csrfToken' => csrf_token(),
+                'siteName'  => config('app.name'),
+                'apiDomain' => config('app.url').'/api'
+            ]) !!}
+        </script>
+    </head>
+    <body>
+        <h1>Laravel</h1>
+
+        <div id="app">
+            <app></app>
+        </div>
+
+        <script src="{{ mix('/js/app.js') }}"></script>
+    </body>
+</html>
