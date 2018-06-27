6fddb245d84549c9e0727ebfeab53e49b76130a7
Enrico Höschler <enrico.hoeschler@gmail.com>
2018-06-27 12:23:37 +0200

    Add Card API tests

--- tests/Feature/CardTest.php
+++ tests/Feature/CardTest.php
@@ -7,59 +7,66 @@ use Illuminate\Foundation\Testing\WithFaker;
 use App\User;
 use App\Board;
 use App\BoardList;
+use App\Card;
 
-class BoardListTest extends DatabaseTestCase
+class CardTest extends DatabaseTestCase
 {
     public function testLoginRequired()
     {
         $user = factory(User::class)->create();
         $board = factory(Board::class)->create(['user_id' => $user->id]);
-        $payload = ['name' => 'My List', 'board_id' => $board->id];
+        $boardList = factory(BoardList::class)->create(['board_id' => $board->id]);
+        $payload = ['name' => 'My Card', 'order' => 1, 'list_id' => $boardList->id];
 
-        $this->json('POST', 'api/boardLists', $payload)
+        $this->json('POST', 'api/cards', $payload)
             ->assertStatus(401);
     }
 
-    public function testUserCanCreateLists()
+    public function testUserCanCreateCards()
     {
         $user = factory(User::class)->create();
         $board = factory(Board::class)->create(['user_id' => $user->id]);
-        $payload = ['name' => 'My List', 'board_id' => $board->id];
+        $boardList = factory(BoardList::class)->create(['board_id' => $board->id]);
+        $payload = ['name' => 'My Card', 'order' => 1, 'list_id' => $boardList->id];
 
         $this->actingAs($user)
-            ->json('POST', 'api/boardLists', $payload)
+            ->json('POST', 'api/cards', $payload)
             ->assertStatus(201)
             ->assertJson(['name' => $payload['name']]);
     }
 
-    public function testUserCanUpdateLists()
+    public function testUserCanUpdateCards()
     {
         $user = factory(User::class)->create();
         $board = factory(Board::class)->create(['user_id' => $user->id]);
         $boardList = factory(BoardList::class)->create(['board_id' => $board->id]);
-        $payload = ['name' => 'My List'];
+        $card = factory(Card::class)->create(['board_list_id' => $boardList->id]);
+        $payload = ['name' => 'My Card'];
 
         $this->actingAs($user)
-            ->json('PATCH', 'api/boardLists/' . $boardList->id, $payload)
+            ->json('PATCH', 'api/cards/' . $card->id, $payload)
             ->assertStatus(200)
             ->assertJson(['name' => $payload['name']])
-            ->assertJsonMissing(['name' => $boardList->name]);
+            ->assertJsonMissing(['name' => $card->name]);
     }
 
-    public function testUserCanDeleteLists()
+    public function testUserCanDeleteCards()
     {
         $user = factory(User::class)->create();
         $board = factory(Board::class)->create(['user_id' => $user->id]);
         $boardList = factory(BoardList::class)->create(['board_id' => $board->id]);
+        $card = factory(Card::class)->create(['board_list_id' => $boardList->id]);
+        $card2 = factory(Card::class)->create(['board_list_id' => $boardList->id]);
 
         $this->actingAs($user)
-            ->json('DELETE', 'api/boardLists/' . $boardList->id)
+            ->json('DELETE', 'api/cards/' . $card->id)
             ->assertStatus(204);
 
         // Check that it is really gone with a GET request of the board of the user
         $this->actingAs($user)
             ->json('GET', 'api/boards/' . $board->id)
-            ->assertJsonCount(0, 'board_lists');
+            ->assertDontSee($card->name)
+            ->assertSee($card2->name);
     }
 
 }
